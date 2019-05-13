2ab33f8a065174178291f8926efe6ab14639ec2c
flep106 <vandrilho@gmail.com>
2019-05-13 19:27:59 -0300

    deixando o código mais semântico

--- src/app.js
+++ src/app.js
@@ -1,34 +1,17 @@
-'use strict'
-const Telegram = require('telegram-node-bot')
-const TelegramBaseController = Telegram.TelegramBaseController
-//pega os comandos dados pelo BOT
-const TextCommand = Telegram.TextCommand
-//TOKEN VandrilhoBot
-const chatbot = new Telegram.Telegram('882177377:AAEe6LJDv6xvVnp3HbuJTt_JsPJx5OQ18eI')
+
+process.env.NTBA_FIX_319 = 1;
 /*
-O método allEventsAction é responsável por enviar a mensagem para o nosso chatbot. Nesse método 
-nós temos o parâmetro scope que foi injetado para nós por conta de estendermos a classe 
-TelegramBaseController.
-Temos também o get routes. Esse é o cara responsável por expor o método allEventsAction 
-para o nosso chatbot, ou seja, ajuda nosso chatbot a executar do lado de fora da controller 
-o método allEventsAction. Para isto, precisaremos chamar a chave que foi atribuída a ela, 
-que é o allEvents. Bora fazer a comunicação do nosso chatbot com a nossa controller:
+node-telegram-bot-api deprecated Automatic enabling of cancellation of promises is deprecated.
+In the future, you will have to enable it yourself.
+See https://github.com/yagop/node-telegram-bot-api/issues/319.
 */
-class EventsController extends TelegramBaseController{
-    //metodo que envia mensagem para o BOT
-    allEventsAction(scope){
-        let msg=`Vectra Consultoria e Serviços - Mensagem enviada por TELEGRAM\n Desenvolvido por Vandrilho`
-        scope.sendMessage(msg)
-    }
-    // expoe o allEventAction para o bot
-    get routes(){
-        return{
-            //mapeamento evento -> Metodo chamado na API node
-            'allEvents': 'allEventsAction'
-        }
-    }
-}
-//mapeamento comandoBot -> Metodo chamado na API TELEGRAM
-chatbot.router
-    .when(new TextCommand('/allevents', 'allEvents'), new EventsController())
+const TelegramBot = require( `node-telegram-bot-api` )
+const TOKEN = '882177377:AAEe6LJDv6xvVnp3HbuJTt_JsPJx5OQ18eI'
+const bot = new TelegramBot( TOKEN, { polling: true } )
+
+
+bot.on('new_chat_members', (msg) =>{
+    console.log(msg)
+    bot.sendMessagee(msg.chat.id, `Olá ${msg.from.first_name},  bem vindo ao grupo de SDM, qualquer dúvida SEMPRE procurar Sandro, nunca fale com Vandrilho :D.`)
+})
 
